56cd84d75b7a69b9f8e1363c8c80c602408ebb07
Asura0107 <mircoorosio@gmail.com>
2023-11-20 16:47:32 +0100

    U3-W1-D1

--- form.js
+++ form.js
@@ -4,22 +4,24 @@ var randomplay = Math.floor(Math.random() * 100);
 var form = document.querySelector(".form");
 form.onsubmit = function (event) {
     event.preventDefault();
+    var value1 = firstplay === null || firstplay === void 0 ? void 0 : firstplay.value;
+    var value2 = secondplay === null || secondplay === void 0 ? void 0 : secondplay.value;
     var difference1;
     var difference2;
-    console.log("il numero scelto dal player 1 è: " + firstplay);
-    console.log("il numero scelto dal player 2 è: " + secondplay);
+    console.log("il numero scelto dal player 1 è: " + value1);
+    console.log("il numero scelto dal player 2 è: " + value2);
     console.log("il numero uscito dal dado è: " + randomplay);
-    if (randomplay > firstplay) {
-        difference1 = randomplay - firstplay;
+    if (randomplay > value1) {
+        difference1 = randomplay - value1;
     }
     else {
-        difference1 = firstplay - randomplay;
+        difference1 = value1 - randomplay;
     }
-    if (randomplay > secondplay) {
-        difference2 = randomplay - secondplay;
+    if (randomplay > value2) {
+        difference2 = randomplay - value2;
     }
     else {
-        difference2 = secondplay - randomplay;
+        difference2 = value2 - randomplay;
     }
     if (difference1 > difference2) {
         console.log("il player 2 è più vicino al risultato");
--- form.ts
+++ form.ts
@@ -1,26 +1,28 @@
-let firstplay:any = document.querySelector("#firstplayer")
-let secondplay:any = document.querySelector("#secondplayer")
+let firstplay:any = document.querySelector("#firstplayer") as HTMLInputElement | null;
+let secondplay:any = document.querySelector("#secondplayer") as HTMLInputElement | null;
 let randomplay: number = Math.floor(Math.random() * 100);
 let form :any = document.querySelector(".form")
 
 form.onsubmit= (event)=> {
     event.preventDefault()
+    let value1 : number = firstplay?.value
+    let value2 : number = secondplay?.value
     var difference1:number;
     var difference2:number;
-    console.log("il numero scelto dal player 1 è: " + firstplay);
-    console.log("il numero scelto dal player 2 è: " +secondplay);
+    console.log("il numero scelto dal player 1 è: " +value1);
+    console.log("il numero scelto dal player 2 è: " +value2);
     console.log("il numero uscito dal dado è: " + randomplay);
-    if (randomplay > firstplay) {
-        difference1 = randomplay - firstplay;
+    if (randomplay >value1) {
+        difference1 = randomplay -value1;
     }
     else {
-        difference1 = firstplay - randomplay;
+        difference1 =value1 - randomplay;
     }
-    if (randomplay >secondplay) {
-        difference2 = randomplay -secondplay;
+    if (randomplay >value2) {
+        difference2 = randomplay -value2;
     }
     else {
-        difference2 =secondplay - randomplay;
+        difference2 =value2 - randomplay;
     }
     if (difference1 > difference2) {
         console.log("il player 2 è più vicino al risultato");
